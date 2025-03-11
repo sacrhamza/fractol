b286a7b696c62daa1bf74b4eb8e41ff9a86529b0
sacrhamza <sacrhamzaschool@gmail.com>
2025-03-11 21:33:19 +0000

    fractol

--- fractol.h
+++ fractol.h
@@ -4,4 +4,39 @@
 # include <stdlib.h>
 # include <mlx.h>
 # include <math.h>
+# define ESC 65307
+# define LARROW 65361
+# define RARROW 65363
+# define UARROW 65362 
+# define DARROW 65364
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	t_data	img;
+}	t_vars;
+
+typedef struct s_dimention
+{
+	int	x_size;
+	int	y_size;	
+}	t_dimention;
+
+typedef struct fractal
+{
+	float	x;
+	float	y;
+}	t_fractal;
+
+void	close_window_and_exit(t_vars *vars);
 #endif
